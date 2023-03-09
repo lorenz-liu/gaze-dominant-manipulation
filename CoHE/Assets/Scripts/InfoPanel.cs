2363925d849299037abcf1e28ef90cad45dacdaa
Zhaoxun Liu <sprlau.me@proton.me>
2023-03-09 12:19:42 +0800

    update

---
+++ CoHE/Assets/Scripts/InfoPanel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+class InfoPanel : MonoBehaviour
+{
+    [SerializeField] public Raycast raycast;
+
+    private void Update()
+    {
+        GetComponent<UnityEngine.UI.Text>().text = GenerateInfo();
+    }
+
+    private string GenerateInfo()
+    {
+        return $"{"Selection Signal: ",20}{(raycast.winking ? "Winking" : "Double Blinking"),10}";
+    }
+}
