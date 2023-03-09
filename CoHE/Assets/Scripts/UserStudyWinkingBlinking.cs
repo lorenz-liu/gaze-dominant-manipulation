741074ee95d4a5fc52d83f5c5b18e6df36f45273
Zhaoxun Liu <sprlau.me@proton.me>
2023-03-09 12:53:53 +0800

    user study winking vs blinking important

--- CoHE/Assets/Scripts/UserStudyWinkingBlinking.cs
+++ CoHE/Assets/Scripts/UserStudyWinkingBlinking.cs
@@ -1,32 +1,61 @@
 ﻿using System;
 using UnityEngine;
+using Random = System.Random;
 
 class UserStudyWinkingBlinking : MonoBehaviour
 {
-    private GameObject _target1;
-    private GameObject _target2;
-    private GameObject _target3;
-    private GameObject _target4;
-    private GameObject _target5;
-    private GameObject _target6;
+    private GameObject _target;
 
     private const float Radius = 4f;
     private const float Distance = 15f;
 
+    private Random _random;
+    private int _interval;
+    private int _countInterval;
+
     private void Start()
     {
-        _target1 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        _target2 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        _target3 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        _target4 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        _target5 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        _target6 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        _random = new Random();
+        _interval = _random.Next(300, 500);
+    }
 
-        _target1.transform.position = new Vector3(0, Radius, Distance);
-        _target2.transform.position = new Vector3(Radius / 2 * (float)Math.Sqrt(3), Radius / 2, Distance);
-        _target3.transform.position = new Vector3(Radius / 2 * (float)Math.Sqrt(3), -Radius / 2, Distance);
-        _target4.transform.position = new Vector3(0, -Radius, Distance);
-        _target5.transform.position = new Vector3(-Radius / 2 * (float)Math.Sqrt(3), -Radius / 2, Distance);
-        _target6.transform.position = new Vector3(-Radius / 2 * (float)Math.Sqrt(3), Radius / 2, Distance);
+    private void CreateRandomTarget()
+    {
+        switch (_random.Next(1, 7))
+        {
+            case 1:
+                _target = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                _target.transform.position = new Vector3(0, Radius, Distance);
+                break;
+            case 2:
+                _target = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                _target.transform.position = new Vector3(Radius / 2 * (float)Math.Sqrt(3), Radius / 2, Distance);
+                break;    
+            case 3:
+                _target = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                _target.transform.position = new Vector3(Radius / 2 * (float)Math.Sqrt(3), -Radius / 2, Distance);
+                break;    
+            case 4:
+                _target = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                _target.transform.position = new Vector3(0, -Radius, Distance);
+                break;    
+            case 5:
+                _target = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                _target.transform.position = new Vector3(-Radius / 2 * (float)Math.Sqrt(3), -Radius / 2, Distance);
+                break;    
+            case 6:
+                _target = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                _target.transform.position = new Vector3(-Radius / 2 * (float)Math.Sqrt(3), Radius / 2, Distance);
+                break;
+                
+        }
+    }
+
+    private void Update()
+    {
+        if (++_countInterval <= _interval) return;
+        CreateRandomTarget();
+        _countInterval = 0;
+        _interval = _random.Next(300, 500);
     }
 }
